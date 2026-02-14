91708fe422248123833e19b18b29c32c403313c1
Reinhard Wansch <reinhard@wansch.at>
2026-02-14 15:34:22 +0100

    roll dice button

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,17 @@ import { useState } from "react"
 import Die from "./Die"
 
 export default function App() {
+    
     const [dice, setDice] = useState(generateAllNewDice())
     
     function generateAllNewDice() {
         return new Array(10)
             .fill(0)
             .map(() => Math.ceil(Math.random() * 6))
+    }
+    
+    function rollDice() {
+        setDice(generateAllNewDice())
     }
     
     const diceElements = dice.map(num => <Die value={num} />)
@@ -17,6 +22,9 @@ export default function App() {
             <div className="dice-container">
                 {diceElements}
             </div>
+            
+            <button className="roll-dice" onClick={rollDice}>Roll</button>
+            
         </main>
     )
 }
